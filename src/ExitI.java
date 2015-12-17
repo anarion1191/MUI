9983187957fa5875be5f8dc48f5f6186106ea118
Kian Nejadfard <k.nejadfard@gmail.com>
2015-12-16 23:17:05 -0500

    Add basic menu item for exit with a dummy action

---
+++ src/ExitI.java
@@ -0,0 +1,8 @@
+public class ExitI extends Item {
+    public ExitI() {
+        name = "Exit";
+    }
+    public void action() {
+        System.out.println("Exit Menu Item Has Been Selected!");
+    }
+}
