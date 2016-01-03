c97d08603c27b910c938e669e97b0fbf29777a2e
Kian Nejadfard <k.nejadfard@gmail.com>
2016-01-03 18:04:05 -0500

    Add javadoc comments

--- src/mui/MUI.java
+++ src/mui/MUI.java
@@ -7,22 +7,50 @@ import mui.exception.ExitMenuException;
 import mui.exception.BadMenuChoiceException;
 import java.util.Scanner;
 
+/**
+ * Represents a Menu User Interface that is to be
+ * extended to create new menus.
+ * In order to create a new menu, just extend {@code MUI}
+ * and override the constructor. The overridden
+ * constructor must call {@code super()} first and then add
+ * the necessary menu entries.
+ * 
+ * @author Kian Nejadfard
+ *
+ */
 public class MUI {
     
+	/**
+	 * A {@code Vector} containing objects of type {@code Item}
+	 * or the subclasses of {@code Item} that construct the
+	 * menu.
+	 */
     private Vector<Item> items;
 
+    /**
+     * Constructor that initializes the member {@code Vector} object.
+     */
     public MUI() {
     	
         items = new Vector<Item>();
-    
+
     }
 
+    /**
+     * Adds a new menu entry to the {@code Vector} that holds
+     * all the items.
+     * 
+     * @param item An object of type {@code Item} or a subclass.
+     */
     public void addItem(Item item) {
         
     	items.add(item);
     
     }
 
+    /**
+     * Prints the menu items to {@code System.out}.
+     */
     public void show() {
     	
     	System.out.println("Choose an item from the menu:");
@@ -32,6 +60,13 @@ public class MUI {
     
     }
     
+    /**
+     * Prints the menu items and gets the user's input
+     * in a loop. After each selection, the item's
+     * {@code action()} member function is called.
+     * 
+     * @throws ExitMenuException If the Exit item is selected.
+     */
     public void run() throws ExitMenuException {
     	
     	while(true) {
