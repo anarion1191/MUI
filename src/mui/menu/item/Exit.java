ce48351c58e9b9583fbf8120aabb88c0d56c456b
Kian Nejadfard <k.nejadfard@gmail.com>
2016-01-05 11:15:42 -0500

    Add javadoc comments

--- src/mui/menu/item/Exit.java
+++ src/mui/menu/item/Exit.java
@@ -1,13 +1,31 @@
 package mui.menu.item;
 
+/**
+ * Class representing the Exit menu item.
+ * The returning value of the {@code action()}
+ * method is used to determine if the program
+ * must be terminated.
+ * 
+ * @author Kian Nejadfard
+ *
+ */
 public class Exit extends Item {
 	
+	/**
+	 * Only need to set the {@code name} field.
+	 */
     public Exit() {
         
     	name = "Exit";
     
     }
     
+    /**
+     * No special action needs to be done.
+     * 
+     * @return false To indicate that the program
+     * must terminate.
+     */
     public boolean action() {
         
     	return false;
